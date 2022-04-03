5436024494294dcfb307f1cc1466380ec6d6e2df
yss <waku.pdxppxzy@gmail.com>
2022-04-03 14:12:41 +0900

    update: add comments

--- lib/async.dart
+++ lib/async.dart
@@ -28,23 +28,30 @@ class Async {
     print("method begin");
     print(DateTime.now().toString());
     print("data1 start");
-    // Future<String> result1 = asyncFunc("data1", 3);
-    // result1.then((result) {
-    //   print(result);
-    // });
-    // print("data2 start");
-    // Future<String> result2 = asyncFunc("data2", 2);
-    // result2.then((result) {
-    //   print(result);
-    // });
-    // print("data3 start");
-    // Future<String> result3 = asyncFunc("data3", 1);
-    // result3.then((result) {
-    //   print(result);
-    // });
-    Future.wait([asyncFunc("data1", 3), asyncFunc("data2", 2), asyncFunc("data3", 1)]).then((result) {
+    // 非同期処理1
+    Future<String> result1 = asyncFunc("data1", 3);
+    result1.then((result) {
       print(result);
     });
+
+    // 非同期処理2
+    print("data2 start");
+    Future<String> result2 = asyncFunc("data2", 2);
+    result2.then((result) {
+      print(result);
+    });
+
+    // 非同期処理3
+    print("data3 start");
+    Future<String> result3 = asyncFunc("data3", 1);
+    result3.then((result) {
+      print(result);
+    });
+
+    // 非同期処理の待ち合わせ
+    // Future.wait([asyncFunc("data1", 3), asyncFunc("data2", 2), asyncFunc("data3", 1)]).then((result) {
+    //   print(result);
+    // });
   }
 
   String syncFunc(String name, int time) {
