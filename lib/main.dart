d023abea507d222c606d25aec9fe866ba4916799
yss <waku.pdxppxzy@gmail.com>
2022-04-03 16:24:20 +0900

    add: stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_zenn_study/BussinessLogic.dart';
+import "dart:async";
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,53 +18,53 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance!.addObserver(this);
-  }
-  @override
-  void dispose() {
-    print("dispose");
-    WidgetsBinding.instance!.removeObserver(this);
-    super.dispose();
-  }
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    print("stete = $state");
-    switch (state) {
-      case AppLifecycleState.inactive:
-        print('非アクティブになったときの処理');
-        break;
-      case AppLifecycleState.paused:
-        print('停止されたときの処理');
-        break;
-      case AppLifecycleState.resumed:
-        print('再開されたときの処理');
-        break;
-      case AppLifecycleState.detached:
-        print('破棄されたときの処理');
-        break;
-    }
-  }
+
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  _incrementCounter() {
+  var intStream = StreamController<int>();
+  var stringStream = StreamController<String>();
+  var generator = new Generator();
+  var coodinator = new Coordinator();
+  var consumer = new Consumer();
+
+  void _incrementCounter() {
+      generator.generate();
     setState(() {
       _counter++;
     });
   }
+
+  @override
+  void initState() {
+      generator.init(intStream);
+      coodinator.init(intStream, stringStream);
+      consumer.init(stringStream);
+      coodinator.coordinate();
+      consumer.consume();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+      intStream.close();
+      stringStream.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(widget.title!),
       ),
       body: Center(
         child: Column(
@@ -72,16 +75,18 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
             ),
             Text(
               '$_counter',
+              key: Key('counter'),
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: Key('increment'),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
