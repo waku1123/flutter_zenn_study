db10af90595add5a1a84df5c787f8bbf83517a49
yss <waku.pdxppxzy@gmail.com>
2022-04-03 15:32:04 +0900

    add: Nullable - Non-NullAble

---
+++ lib/NullSample.dart
@@ -0,0 +1,33 @@
+import 'dart:math' as math;
+
+class NullSample {
+  mehod1() {
+    int x = 10; // nullの可能性なし
+    int? y = getNullable(); // nullの可能性あり
+
+    // x = y; // Non-Nullable に Nullable を代入するとコンパイルエラー
+    // y = x; // Nullable に Non=Nullable を代入するのはOK
+    if (y != null) {
+      x = y; // null チェックの後なのでコンパイルOK
+    }
+    print("method1 - x: $x, y: $y");
+  }
+
+  method2() {
+    int x = 20ｄ;
+    int? y = getNullable();
+
+    x = y!; // yをNullableにキャストして代入する
+    print("method2 - x: $x, y: $y");
+  }
+
+  // random でnull か 1 を返す
+  getNullable() {
+    var rand = math.Random();
+    if (rand.nextInt(2) == 0) {
+      return null;
+    } else {
+      return 1;
+    }
+  }
+}
