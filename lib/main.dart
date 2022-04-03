4e5fe51007ca9064fe672033812445e282915fb1
yss <waku.pdxppxzy@gmail.com>
2022-04-03 16:06:46 +0900

    add: app lifecycle (focus event)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_zenn_study/dummy.dart';
 void main() {
   runApp(MyApp());
 }
@@ -16,52 +16,52 @@ class MyApp extends StatelessWidget {
   }
 }
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-  final String? title;
-
-  // statefulWidgetを構築するとすぐに呼ばれる(Widgetツリーの生成のため)
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      // 状態の変更をFrameworkに通知する
-      print("call setState");
-      _counter++;
-    });
-    nextpage();
-  }
-
-  void nextpage() async {
-      {
-        // ダミー画面へ遷移
-        await Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {
-          return DummyPage();
-      }));
-      }
-    }
-
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   @override
   void initState() {
-    // Widgetツリーの初期化を行う
-    print("call initState");
     super.initState();
+    WidgetsBinding.instance!.addObserver(this);
   }
   @override
-  void didChangeDependencies() {
-    // stateオブジェクトの依存関係が変更されたときに呼ばれる
-    print("call didChangeDependencies");
-    super.didChangeDependencies();
+  void dispose() {
+    print("dispose");
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("stete = $state");
+    switch (state) {
+      case AppLifecycleState.inactive:
+        print('非アクティブになったときの処理');
+        break;
+      case AppLifecycleState.paused:
+        print('停止されたときの処理');
+        break;
+      case AppLifecycleState.resumed:
+        print('再開されたときの処理');
+        break;
+      case AppLifecycleState.detached:
+        print('破棄されたときの処理');
+        break;
+    }
+  }
+  int _counter = 0;
+  _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
   @override
   Widget build(BuildContext context) {
-    // Widgetで構成されるUIを構築する。
-    print("call build");
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title!),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
@@ -72,36 +72,16 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              key: Key('counter'),
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        key: Key('increment'),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
-  }
-  @override
-  void didUpdateWidget(oldWidget) {
-    // Widgetの構成が変更される度に呼ばれる
-    print("call didUpdateWidget");
-    super.didUpdateWidget(oldWidget);
-  }
-  @override
-  void deactivate() {
-    // stateオブジェクトがツリーから削除される度に呼ばれる
-    print("call deactivate");
-     super.deactivate();
-  }
-  @override
-  void dispose() {
-    // オブジェクトがツリーから完全に削除されビルドされなくなったら呼ばれる
-    print("call dispose");
-    super.dispose();
   }
 }
