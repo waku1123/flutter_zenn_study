44f7ca43b22893c907125dad45ba0aaa6a64fac6
yss <waku.pdxppxzy@gmail.com>
2022-04-05 23:02:28 +0900

    update: about BuildContext

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zenn_study/BusinessLogic.dart';
-import "dart:async";
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,48 +16,22 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, this.title}) : super(key: key);
   final String? title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var intStream = StreamController<int>();
-  var stringStream = StreamController<String>.broadcast();
-  var generator = new Generator();
-  var coodinator = new Coordinator();
-  var consumer = new Consumer();
-
   void _incrementCounter() {
-    generator.generate();
+    print(Theme.of(context));
+    print(Theme.of(context).primaryColor);
+    print(Theme.of(context).brightness);
     setState(() {
-      _counter++;
+      _counter++; // ここにブレイクポイントをはる
     });
   }
-
-  @override
-  void initState() {
-    generator.init(intStream);
-    coodinator.init(intStream, stringStream);
-    consumer.init(stringStream);
-    coodinator.coordinate();
-    consumer.consume();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    intStream.close();
-    stringStream.close();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,30 +39,20 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title!),
       ),
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('You have pushed the button this many times:',),
-              Text(
-                '$_counter',
-                key: Key('counter'),
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              StreamBuilder<String>(
-                stream: stringStream.stream,
-                initialData: "",
-                builder: (context, snapshot) {
-                  return Text(
-                    'RANDOM : ${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                },
-              ),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        key: Key('increment'),
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
